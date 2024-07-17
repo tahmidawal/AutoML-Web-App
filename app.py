127ca9a686666cc105981e2db68d9dc23d0ad83d
tahmidawal <98552052+tahmidawal@users.noreply.github.com>
2024-07-17 15:28:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ import pandas as pd
 import os
 from pycaret.classification import setup, compare_models, pull, save_model, load_model
 
+def preprocess_data(df):
+    # Convert "% of replacement" column to numeric type
+    df['% of replacement'] = pd.to_numeric(df['% of replacement'], errors='coerce')
+    return df
 
 if os.path.exists("dataset.csv"):
     df = pd.read_csv("dataset.csv", index_col=None)
+    df = preprocess_data(df)
 
 with st.sidebar:
     st.image("https://www.onepointltd.com/wp-content/uploads/2019/12/shutterstock_1166533285-Converted-02.png")
@@ -13,16 +18,14 @@ with st.sidebar:
     choice = st.radio("Select the task", ["Upload", "Modeling", "Download"])
     st.info("This project application helps you build and explore your data")
     
-    
 if choice == "Upload":
     st.title("Upload Your Dataset")
     file = st.file_uploader("Upload Your Dataset")
     if file:
         df = pd.read_csv(file, index_col=None)
+        df = preprocess_data(df)
         df.to_csv("dataset.csv", index=None)
         st.dataframe(df)
-
-
 
 if choice == "Modeling":
     chosen_target = st.selectbox("Select the target column", df.columns)
